9d45b828c4176c2fd05cb98f7fe1663d2dd852d4
izumi.haruno <izumi.haruno@hamee.co.jp>
2024-09-23 05:52:29 +0900

    整形

--- pixabay/lib/main.dart
+++ pixabay/lib/main.dart
@@ -40,12 +40,12 @@ class _PixabayPageState extends State<PixabayPage> {
 
   Future<void> fetchImages(String text) async {
     final Response response = await Dio().get(
-        'https://pixabay.com/api/',
-        queryParameters: {
-          'key' : dotenv.get('API_KEY'),
-          'q' : text,
-          'image_type' : 'photo'
-        },
+      'https://pixabay.com/api/',
+      queryParameters: {
+        'key': dotenv.get('API_KEY'),
+        'q': text,
+        'image_type': 'photo'
+      },
     );
     final List hits = response.data['hits'];
     setState(() {
@@ -149,9 +149,15 @@ class PixabayImage {
   final int likes;
   final String webformatURL;
 
-  PixabayImage({required this.previewURL, required this.likes, required this.webformatURL});
+  PixabayImage(
+      {required this.previewURL,
+      required this.likes,
+      required this.webformatURL});
 
   factory PixabayImage.fromMap(Map<String, dynamic> map) {
-    return PixabayImage(previewURL: map['previewURL'], likes: map['likes'], webformatURL: map['webformatURL']);
+    return PixabayImage(
+        previewURL: map['previewURL'],
+        likes: map['likes'],
+        webformatURL: map['webformatURL']);
   }
 }
