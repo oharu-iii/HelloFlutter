0d8567507a737603e61ba3382f413decb8770676
izumi.haruno <izumi.haruno@hamee.co.jp>
2025-02-11 19:14:49 +0900

    イベントタイトルを一覧表示

--- life_counter/lib/main.dart
+++ life_counter/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:life_counter/life_event.dart';
+import 'package:life_counter/objectbox.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,8 +29,35 @@ class LifeCounterPage extends StatefulWidget {
 }
 
 class _LifeCounterPageState extends State<LifeCounterPage> {
+  Store? store;
+  Box<LifeEvent>? lifeEventBox;
+  List<LifeEvent> lifeEvents = [];
+
+  Future<void> initialize() async {
+    store = await openStore();
+    lifeEventBox = store?.box<LifeEvent>();
+    lifeEvents = lifeEventBox?.getAll() ?? [];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('人生カウンター'),
+      ),
+      body: ListView.builder(
+        itemCount: lifeEvents.length,
+        itemBuilder: (context, index) {
+          final lifeEvent = lifeEvents [index];
+          return Text(lifeEvent.title);
+        }
+      ),
+    );
   }
 }
